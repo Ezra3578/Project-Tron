3fd36a7aeda790b1a4cf18c60c0fca6c8cd76434
Kcir323 <ericksbp23@gmail.com>
2025-04-15 18:49:17 -0500

    I have created the LightTrail class for the light trail, which works with a finite array of positions

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 from game import TronGame
+from LightTrail import LightTrail
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -14,12 +15,23 @@ tron_game = TronGame()
 tron_game.player1.direction.x = 1
 tron_game.player2.direction.x = -1
 
+trail1 = LightTrail(tron_game.player1, "RED")
+trail2 = LightTrail(tron_game.player2, "BLUE")
+
 while tron_game.running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             tron_game.running = False
 
     tron_game.screen.fill((0, 0, 0))  # <--- limpia la pantalla
+
+    trail1.updateTrail()
+    trail2.updateTrail()
+
+    trail1.drawTrail(tron_game.screen)
+    trail2.drawTrail(tron_game.screen)
+
+    
 
     tron_game.player1.draw_player()
     tron_game.player2.draw_player()
