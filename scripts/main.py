f48c00b93cc06741425e0fa0c850f12098f4f86a
Kcir323 <ericksbp23@gmail.com>
2025-04-15 21:44:39 -0500

    the 8-level tensor (build_Obs_Matrix()) was created for the AI agent to read the state of the board

--- scripts/main.py
+++ scripts/main.py
@@ -1,11 +1,11 @@
 import pygame
 from pygame.locals import *
 from game import TronGame
-from LightTrail import LightTrail
+
 
 pygame.init()
 clock = pygame.time.Clock()
-#running = True
+
 pygame.display.set_caption("TRON")
 dt = 0
 
@@ -15,8 +15,7 @@ tron_game = TronGame()
 tron_game.player1.direction.x = 1
 tron_game.player2.direction.x = -1
 
-trail1 = LightTrail(tron_game.player1, "RED")
-trail2 = LightTrail(tron_game.player2, "BLUE")
+
 
 while tron_game.running:
     for event in pygame.event.get():
@@ -24,14 +23,6 @@ while tron_game.running:
             tron_game.running = False
 
     tron_game.screen.fill((0, 0, 0))  # <--- limpia la pantalla
-
-    trail1.updateTrail()
-    trail2.updateTrail()
-
-    trail1.drawTrail(tron_game.screen)
-    trail2.drawTrail(tron_game.screen)
-
-    
 
     tron_game.player1.draw_player()
     tron_game.player2.draw_player()
