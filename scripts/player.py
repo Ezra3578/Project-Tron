3fb64b44d099413212de41a25843ba18dd4d970a
Ezra3578 <juanjimenezp35@gmail.com>
2025-05-31 17:30:47 -0500

    Modified the movement by squares and lighttrails by time

--- scripts/player.py
+++ scripts/player.py
@@ -2,33 +2,42 @@ import pygame
 from pygame.math import Vector2
 
 class Player:
-    def __init__(self, x, y, screen, color):
-        self.position = pygame.Vector2(x, y) #posición en cada frame
-        self.speed = 4  # Velocidad de movimiento
+    def __init__(self, x, y, screen, color, cell_size, key_mapping):
+        self.position = Vector2(x, y) #posición en cada frame
+        self.old_position = Vector2(x,y) #posicion anterior
+        self.direction = Vector2(0, 0) #vector dirección al que apunta
+
         self.screen = screen
         self.color = color
-        self.direction = Vector2(0, 0) #vector dirección al que apunta
-        self.size = 20
 
-    def move(self, keys, key_mapping):
-        # key_mapping va a ser un diccionario con las teclas para cada dirección:
-        new_direction = Vector2(0, 0) #para realizar el cambio de dirección una vez se cambie la tecla
+        self.cell_size = cell_size
+        self.size = self.cell_size // 2
         
-        if keys[key_mapping['left']] and self.direction.x != 1: #por ejemplo key_mapping['left'] va a ser pygame.k_left para player 2 y pygame.k_a para player 1
-            new_direction.x = -1
-        elif keys[key_mapping['right']]and self.direction.x != -1:
-            new_direction.x = 1
-        elif keys[key_mapping['up']]and self.direction.y != 1:
-            new_direction.y = -1
-        elif keys[key_mapping['down']]and self.direction.y != -1:
-            new_direction.y = 1
+        self.move_delay = 150 #ms entre movimientos
+        self.time_since_move = 0 #cuenta el tiempo desde que se mueve
 
-        if new_direction.length() > 0:
-            self.direction = new_direction.normalize()
+        self.key_mapping = key_mapping
 
-        self.position += self.direction * self.speed    #actualiza la posición
+    def move(self, dt):
+
+        self.time_since_move += dt
+
+        
+
+        if self.time_since_move >= self.move_delay:
+            self.time_since_move = 0
+            self.old_position = self.position.copy() #se guarda la posicion antes de moverse para luego pasarla al trazo de luz
+            self.position += self.direction    #actualiza la posición
+        
+
+    def change_direction(self, new_dir):
+        # Cambia de dirección si no es la contraria
+        if new_dir.x != -self.direction.x and new_dir.y != -self.direction.y:
+            self.direction = new_dir
 
     def draw_player(self):
-        pygame.draw.circle(self.screen, self.color, (int(self.position.x), int(self.position.y)), self.size)
+        #drawing now by squares and not pixels. Although, to calculate where is it drawn it must be focused on pixels 
+        pixel_x = int(self.position.x * self.cell_size + self.cell_size // 2) #puts the center of the player on the center of the map square
+        pixel_y = int(self.position.y * self.cell_size + self.cell_size // 2)
+        pygame.draw.circle(self.screen, self.color, (pixel_x, pixel_y), self.size - 2)
 
-    
