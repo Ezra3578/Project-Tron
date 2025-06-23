65a1cda52b2a2cd69d453b1fd0b4e0738b21794d
Kcir323 <ericksbp23@gmail.com>
2025-06-22 23:55:24 -0500

    Added players 3 and 4, modified obs tensor

--- scripts/player.py
+++ scripts/player.py
@@ -4,7 +4,8 @@ import numpy as np
 import math
 
 class Player:
-    def __init__(self, x, y, screen, color, cell_size, key_mapping):
+    def __init__(self, x, y, screen, color, cell_size, key_mapping, team = "RED" or "BLUE"):
+        
         self.position = Vector2(x, y) #posición en cada frame
         self.old_position = Vector2(x,y) #posicion anterior
         self.direction = Vector2(0, 0) #vector dirección al que apunta
@@ -18,7 +19,9 @@ class Player:
 
         self.cell_size = cell_size
         self.size = self.cell_size // 2
-        
+
+        self.team = team
+
         self.move_delay = 150 #ms entre movimientos
         self.time_since_move = 0 #cuenta el tiempo desde que se mueve
 
@@ -28,8 +31,14 @@ class Player:
     def getTrailEstate(self):
         return self.trailEnabled
     
+    def isDead(self):
+        if not self.isAlive:
+            print(f"Jugador {self.color} ha muerto")
+            
 
     def move(self, dt):
+        if not self.isAlive:
+            return  # No se mueve si está muerto
 
         self.time_since_move += dt
 
@@ -41,15 +50,20 @@ class Player:
            
 
     def change_direction(self, new_dir):
+        if not self.isAlive:
+            return  # No cambia de dirección si está muerto
         # Cambia de dirección si no es la contraria
         if new_dir.x != -self.direction.x and new_dir.y != -self.direction.y:
             self.direction = new_dir
 
     def draw_player(self):
+        if not self.isAlive:
+            return  # No dibuja si está muerto
         #drawing now by squares and not pixels. Although, to calculate where is it drawn it must be focused on pixels 
         pixel_x = int(self.position.x * self.cell_size + self.cell_size // 2) #puts the center of the player on the center of the map square
         pixel_y = int(self.position.y * self.cell_size + self.cell_size // 2)
         pygame.draw.circle(self.screen, self.color, (pixel_x, pixel_y), self.size - 2)
+
 
     def get_cone_vision(self, grid_cols, grid_rows, obstacles, fov_deg=90, max_distance=6, num_rays=15):
         """
@@ -84,7 +98,5 @@ class Player:
                 else:
                     break
         return vision # set de coordenadas (x, y) VISIBLES en el cono de visión pero sin iformación de si es estela, muro o jugador
-
-
 
 
